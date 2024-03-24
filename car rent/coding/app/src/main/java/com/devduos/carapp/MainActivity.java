d62dc13370b8bdbf27eb0061270d6b23a169843e
vedant122 <kulkarnivedant123@gmail.com>
2024-03-25 02:27:36 +0530

    commit

---
+++ car rent/coding/app/src/main/java/com/devduos/carapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.devduos.carapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.car1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+            }
+        });
+    }
+}
