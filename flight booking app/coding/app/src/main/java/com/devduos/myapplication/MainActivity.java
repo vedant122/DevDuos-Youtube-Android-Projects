aa84b041573d6210a85ad6f6c62df967f1265180
vedant122 <kulkarnivedant123@gmail.com>
2024-03-25 02:46:43 +0530

    commit

---
+++ flight booking app/coding/app/src/main/java/com/devduos/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.devduos.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.getstarted).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+            }
+        });
+    }
+}
